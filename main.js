3c9c11115de47b9864b9738c410c438e3eccf58a
Welington <welingtoncharnetjr@gmail.com>
2025-05-12 22:05:44 -0300

    fazendo o template

---
+++ main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron');
+
+//Função para criar a janela principal
+function createWindow() {
+
+    // Criar uma nova instancia do BrowserWindow
+    const window = new BrowserWindow({
+
+        width: 800,
+        height: 600,
+    })
+
+    //Carregar o arquivo HTML na janela
+    window.loadFile('index.html');
+}
+
+
+// Evento que é acionado quando o Electron está pronto
+app.whenReady().then(() => {
+    createWindow(); //Chama a função de criar a janela
+})
